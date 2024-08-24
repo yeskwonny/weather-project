91c153b1f62129e8daa2f7189fb0883354d8523d
yesol <yeahsol.kwon@gmail.com>
2024-08-24 20:54:50 +1000

    update the answers up to find_max

--- weather.py
+++ weather.py
@@ -1,6 +1,5 @@
 import csv
-from datetime import datetime
-
+from datetime import datetime 
 DEGREE_SYMBOL = u"\N{DEGREE SIGN}C"
 
 
@@ -24,8 +23,10 @@ def convert_date(iso_string):
     Returns:
         A date formatted like: Weekday Date Month Year e.g. Tuesday 06 July 2021
     """
-    pass
-
+    
+    date = datetime.fromisoformat(iso_string)
+    return date.strftime('%A %d %B %Y')
+        
 
 def convert_f_to_c(temp_in_fahrenheit):
     """Converts a temperature from Fahrenheit to Celcius.
@@ -35,7 +36,10 @@ def convert_f_to_c(temp_in_fahrenheit):
     Returns:
         A float representing a temperature in degrees Celcius, rounded to 1 decimal place.
     """
-    pass
+
+    change_to_float = float(temp_in_fahrenheit)
+    celsius = (change_to_float - 32) * 5 / 9
+    return round(celsius, 1)
 
 
 def calculate_mean(weather_data):
@@ -46,8 +50,15 @@ def calculate_mean(weather_data):
     Returns:
         A float representing the mean value.
     """
-    pass
-
+    # check the number of item in a list
+    length=len(weather_data)
+    total=0
+    #loop and add all value
+    for degree in weather_data:
+        total=total+float(degree)
+    mean=total/length
+    # return mean
+    return mean
 
 def load_data_from_csv(csv_file):
     """Reads a csv file and stores the data in a list.
@@ -57,8 +68,21 @@ def load_data_from_csv(csv_file):
     Returns:
         A list of lists, where each sublist is a (non-empty) line in the csv file.
     """
-    pass
-
+    new_data_list=[]
+  
+    with open(csv_file,mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+    #  skip the header
+        next(csv_reader)
+        for row in csv_reader:
+    #get rid of empty row
+           if(row !=[]):
+            new_data_list.append(row)
+    #  change string into int      
+        for i in range(len(new_data_list)):
+            new_data_list[i][1] = int(new_data_list[i][1])
+            new_data_list[i][2] = int(new_data_list[i][2])
+    return new_data_list
 
 def find_min(weather_data):
     """Calculates the minimum value in a list of numbers.
@@ -68,7 +92,28 @@ def find_min(weather_data):
     Returns:
         The minimum value and it's position in the list. (In case of multiple matches, return the index of the *last* example in the list.)
     """
-    pass
+    # check a list is empty or not 
+    if weather_data !=[]:
+        min_value=weather_data[0]
+        min_index=0
+        for i in range(1,len(weather_data)):
+        # compare 1st element and 2nd element
+        # if weather[i] is smaller than min_value
+        # it becomes min_value
+        # and give the position
+
+            if(weather_data[i]<=min_value):
+                min_value=weather_data[i]
+                min_index=i
+        return (float(min_value),min_index)
+    else:
+        return()
+   
+
+# print(find_min([]))
+
+
+
 
 
 def find_max(weather_data):
@@ -79,7 +124,18 @@ def find_max(weather_data):
     Returns:
         The maximum value and it's position in the list. (In case of multiple matches, return the index of the *last* example in the list.)
     """
-    pass
+    if(weather_data !=[]):
+        max_value=weather_data[0]
+        max_index=0
+        for i in range(1,len(weather_data)):
+            if(weather_data[i]>=max_value):
+                max_value=weather_data[i]
+                max_index=i
+        return (float(max_value),max_index)
+    else:
+        return ()
+    
+print(find_max([49, 57, 56, 55, 57, 53, 49]))
 
 
 def generate_summary(weather_data):
